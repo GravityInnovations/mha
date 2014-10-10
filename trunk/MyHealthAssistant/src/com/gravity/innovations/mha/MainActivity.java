06aea871b20933ad81afc5a7555fe4d2fcbe3cb9
Faik Malik <faik.malik89@gmail.com>
2014-10-10 17:52:12 +0500

    t

--- trunk/MyHealthAssistant/src/com/gravity/innovations/mha/MainActivity.java
+++ trunk/MyHealthAssistant/src/com/gravity/innovations/mha/MainActivity.java
@@ -17,7 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-//test
+//test2
 public class MainActivity extends ActionBarActivity implements
 		ActionBar.TabListener {
 
